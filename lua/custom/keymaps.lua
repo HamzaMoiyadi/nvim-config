afb28e2a1ac6030e9cd643b6257163a24550f6dc
Hamza Moiyadi <hamza.moiyadi@gmail.com>
2023-07-20 01:15:26 +0530

    added keymaps for navigating through tmux and toggling nvim-tree

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -11,3 +11,12 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Switching panes (specifically for tmux)
+vim.keymap.set('n', '<C-h>', "<cmd> TmuxNavigateLeft<CR>", { desc = "Focus on Window Left" })
+vim.keymap.set('n', '<C-l>', "<cmd> TmuxNavigateRight<CR>", { desc = "Focus on Window Right" })
+vim.keymap.set('n', '<C-j>', "<cmd> TmuxNavigateDown<CR>", { desc = "Focus on Window Down" })
+vim.keymap.set('n', '<C-k>', "<cmd> TmuxNavigateUp<CR>", { desc = "Focus on Window Up" })
+
+-- Toggle NvimTree easily
+vim.keymap.set('n', '<C-n>', "<cmd> NvimTreeToggle<CR>", { desc = "Toggle [n]vimTree" })
