e528c542217140d8366b40cbae87893996513174
Hamza Moiyadi <hamza.moiyadi@gmail.com>
2023-07-26 00:01:53 +0530

    added lualine config to look pretty, added new catppuccin theme and configured it, added option to remove mode information

--- init.lua
+++ init.lua
@@ -128,19 +128,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'horizon',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
 
   {
     -- Add indentation guides even on blank lines
--- lua/themes/gruvbox.lua
+++ lua/themes/gruvbox.lua
@@ -2,10 +2,4 @@
 
 return {
 	"morhetz/gruvbox",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		-- load the colorscheme here
-		vim.cmd([[colorscheme gruvbox]])
-	end,
 }
